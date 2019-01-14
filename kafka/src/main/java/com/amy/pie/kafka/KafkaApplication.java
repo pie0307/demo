3649d23732d0032a17a746206e66a0e34ac9da39
liuby <liuby@ziroom.com>
2019-01-14 17:25:15 +0800

    kafka

---
+++ kafka/src/main/java/com/amy/pie/kafka/KafkaApplication.java
@@ -0,0 +1,18 @@
+package com.amy.pie.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author : Administrator.
+ * @Description :
+ * @Date : Created in 2019/1/6 21:42
+ * @Modified By :
+ */
+
+@SpringBootApplication
+public class KafkaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaApplication.class, args);
+    }
+}
---
+++ kafka/src/main/java/com/amy/pie/kafka/produce/KafkaProducer.java
@@ -0,0 +1,49 @@
+package com.amy.pie.kafka.produce;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.FailureCallback;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.SuccessCallback;
+
+import javax.annotation.Resource;
+import java.util.UUID;
+
+/**
+ * 生产者
+ * 使用@EnableScheduling注解开启定时任务
+ */
+@Component
+@EnableScheduling
+@Slf4j
+public class KafkaProducer {
+
+    @Resource
+    private KafkaTemplate kafkaTemplate;
+
+    /**
+     * 定时任务
+     * D:\kafka2.12>.\bin\windows\kafka-server-start.bat .\config\server.properties
+     */
+    @Scheduled(cron = "00/1 * * * * ?")
+    public void send() {
+        String message = UUID.randomUUID().toString();
+        ListenableFuture future = kafkaTemplate.send("test.topic", message);
+        future.addCallback(new SuccessCallback() {
+
+            @Override
+            public void onSuccess(Object o) {
+                log.info("success  ", o);
+            }
+        }, new FailureCallback() {
+            @Override
+            public void onFailure(Throwable throwable) {
+                log.info("error  ", throwable.getMessage());
+            }
+        });
+    }
+
+}
